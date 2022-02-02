d6c9d3a28f3aef59ee68bda349124f6832686aa9
hamzashan123 <hamzashan123@gmail.com>
2022-02-02 22:33:19 +0500

    update

--- modules/Layout/app.blade.php
+++ modules/Layout/app.blade.php
@@ -156,43 +156,28 @@
     @php event(new \Modules\Layout\Events\LayoutEndBody()); @endphp
 
 
-    {{-- <script>
-        $(document).ready(function(){
-            $('.carousel').slick({
-                speed: 500,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots:false,
-                centerMode: true,
-                responsive: [{
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1,
-                        // centerMode: true,    
-                    }
-            
-                }, {
-                    breakpoint: 800,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2,
-                        dots: false,
-                        infinite: true,
-            
-                    }
-                },  {
-                    breakpoint: 500,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        dots: false,
-                        infinite: true,
-                    }
-                }]
-            });
-        });
-          --}}
-    </script>
+   
   </body>
+  <script>
+
+       
+    $('.mainslick').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+      
+
+    </script>
 </html>
--- modules/Space/Views/frontend/layouts/details/space-detail.blade.php
+++ modules/Space/Views/frontend/layouts/details/space-detail.blade.php
@@ -1,31 +1,5 @@
 
-
-
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-
-    <script>
-
-       
-    $('.mainslick').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:2
-        }
-    }
-})
-      
-
-    </script>
+   
     
 <style>
     .promotions {
